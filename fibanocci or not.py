bdecc85ddac3939fd7802e448b14bc89977d95d8
Aarunya Retheep <aarunyaretheep003@gmail.com>
2026-02-19 20:53:10 +0530

    Add files via upload

---
+++ fibanocci or not.py
@@ -0,0 +1,11 @@
+import math
+
+def is_perfect_square(x):
+    s = int(math.sqrt(x))
+    return s * s == x
+
+def is_fibonacci(n):
+    return is_perfect_square(5*n*n + 4) or \
+           is_perfect_square(5*n*n - 4)
+
+print(is_fibonacci(34))
